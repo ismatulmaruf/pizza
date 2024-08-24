9f2fd3212a840dd66fe8480c1768c31e2aa1096a
Md Maruf <maruf509599@gmail.com>
2024-08-24 21:04:15 +0600

    as

--- src/components/menu/AddToCartButton.js
+++ src/components/menu/AddToCartButton.js
@@ -1,4 +1,4 @@
-import FlyingButton from "react-flying-item";
+// import FlyingButton from "react-flying-item";
 
 export default function AddToCartButton({
   hasSizesOrExtras,
@@ -9,9 +9,9 @@ export default function AddToCartButton({
   if (!hasSizesOrExtras) {
     return (
       <div className="flying-button-parent mt-4">
-        <FlyingButton targetTop={"5%"} targetLeft={"95%"} src={image}>
-          <div onClick={onClick}>Add to cart ${basePrice}</div>
-        </FlyingButton>
+        {/* <FlyingButton targetTop={"5%"} targetLeft={"95%"} src={image}> */}
+        <div onClick={onClick}>Add to cart ${basePrice}</div>
+        {/* </FlyingButton> */}
       </div>
     );
   }
